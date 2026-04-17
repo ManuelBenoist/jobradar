c8ddaf1014ae8855a9d6458814bd9e97af2b9dcb
Manuel Benoist <manuelbenoist@gmail.com>
2026-04-17 18:00:43 +0200

    feat: rename processed_jobs to silver_jobs and update schema for Athena integration

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -3,9 +3,11 @@ resource "aws_glue_catalog_database" "jobradar_db" {
   name = "jobradar_db"
 }
 
-# 2. La Table Processed (format Parquet)
+# 2. La Table Silver (Anciennement processed_jobs)
+# On garde le nom de ressource Terraform "processed_jobs" pour éviter de tout recréer,
+# mais on change le nom réel dans AWS pour "silver_jobs".
 resource "aws_glue_catalog_table" "processed_jobs" {
-  name          = "processed_jobs"
+  name          = "silver_jobs" 
   database_name = aws_glue_catalog_database.jobradar_db.name
 
   table_type = "EXTERNAL_TABLE"
@@ -15,18 +17,19 @@ resource "aws_glue_catalog_table" "processed_jobs" {
   }
 
   storage_descriptor {
-    location      = "s3://jobradar-processed-manuel-cloud/"
+    # CRITIQUE : On pointe bien sur le sous-dossier défini dans ton script Spark
+    location      = "s3://jobradar-processed-manuel-cloud/silver_jobs/"
     input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
     output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
 
     ser_de_info {
-      name                  = "my-stream"
+      name                  = "parquet-ser-de"
       serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
     }
 
-    # Définition des colonnes
+    # --- Mapping exact de sortie Spark ---
     columns {
-      name = "id"
+      name = "job_id"
       type = "string"
     }
     columns {
@@ -34,24 +37,57 @@ resource "aws_glue_catalog_table" "processed_jobs" {
       type = "string"
     }
     columns {
-      name = "company"
+      name = "company_name"
+      type = "string"
+    }
+    columns {
+      name = "location_clean"
       type = "string"
     }
     columns {
       name = "description"
       type = "string"
     }
+    columns {
+      name = "created_at"
+      type = "string"
+    }
+    columns {
+      name = "source_name"
+      type = "string"
+    }
+    columns {
+      name = "extracted_skills"
+      type = "array<string>" # Type complexe pour Athena
+    }
+    columns {
+      name = "salary_min_numeric"
+      type = "int"
+    }
+    columns {
+      name = "is_junior"
+      type = "boolean"
+    }
+    columns {
+      name = "is_senior"
+      type = "boolean"
+    }
+    columns {
+      name = "is_red_flag"
+      type = "boolean"
+    }
+    columns {
+      name = "is_ethical"
+      type = "boolean"
+    }
   }
 
-  # PARTITIONNEMENT : C'est ici qu'on optimise les coûts
-  # On dit à Athena que les données sont rangées dans des dossiers par date
   partition_keys {
     name = "ingestion_date"
     type = "string"
   }
 }
 
-# Sécurisation FinOps : on limite le nombre de requêtes simultanées et la quantité de données scannées
 resource "aws_athena_workgroup" "jobradar_workgroup" {
   name = "jobradar_workgroup"
 
@@ -63,8 +99,6 @@ resource "aws_athena_workgroup" "jobradar_workgroup" {
       output_location = "s3://${aws_s3_bucket.athena_results.bucket}/"
     }
 
-    # Limite de scan par requête : 100 Mo (0.1 Go)
-    # Si une requête tente de scanner plus, elle est coupée net.
     bytes_scanned_cutoff_per_query = 104857600 
   }
 
